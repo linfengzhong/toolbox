11e7ab1a90dfdb51a73792e631ddef88abbb0744
linfengzhong <fred.zhong@outlook.com>
2021-05-09 09:04:27 +0800

    NRPE Manual added

--- Nagios/install_nrpe.sh
+++ Nagios/install_nrpe.sh
@@ -5,7 +5,9 @@
 #Security-Enhanced Linux
 #This guide is based on SELinux being disabled or in permissive mode. Steps to do this are as follows.
 echo "开始安装Nagios NRPE"
+sleep 3
 echo "Step1: SELINUX Disable"
+sleep 3
 sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 setenforce 0
 if ! command; then echo "Step1 failed"; exit 1; fi
@@ -14,6 +16,7 @@ if ! command; then echo "Step1 failed"; exit 1; fi
 #Perform these steps to install the pre-requisite packages.
 #===== RHEL 5/6/7 | CentOS 5/6/7 | Oracle Linux 5/6/7 =====
 echo "Step2: 下载nrpe-4.0.3到tmp文件夹"
+sleep 3
 cd /tmp
 wget https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-4.0.3/nrpe-4.0.3.tar.gz
 tar xzf nrpe-4.0.3.tar.gz
@@ -22,6 +25,7 @@ if ! command; then echo "Step2 failed"; exit 1; fi
 # -------------------------------------------------------------
 #NPRE Installation
 echo "Step3: 安装nrpe，设置用户和用户组、并初始化和启动nrpe服务"
+sleep 3
 ./configure
 make all
 make install-groups-users
@@ -35,6 +39,7 @@ if ! command; then echo "Step3 failed"; exit 1; fi
 #firewall enable port 5666
 #===== RHEL 7/8 | CentOS 7/8 | Oracle Linux 7/8 =====
 echo "Step4: 设置防火墙开启端口5666"
+sleep 3
 firewall-cmd --zone=public --add-port=5666/tcp
 firewall-cmd --zone=public --add-port=5666/tcp --permanent
 if ! command; then echo "Step4 failed"; exit 1; fi
@@ -42,6 +47,8 @@ if ! command; then echo "Step4 failed"; exit 1; fi
 # 这里是判断上条命令是否执行成功的语句块
 if [ $? -eq 0 ]; then
    echo "nrpe 安装成功！"
+   sleep 3
 else
    echo "nrpe 安装失败！"
+   sleep 3
 fi
