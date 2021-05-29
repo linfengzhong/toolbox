8fc166dea868c0e77c9d1994df13a82b274b9037
linfengzhong <fred.zhong@outlook.com>
2021-05-29 21:02:15 +0800

    fix

---
+++ Shell/setup-new-s.sh
@@ -0,0 +1,125 @@
+#!/usr/bin/env bash
+# Author: Linfeng Zhong (Fred)
+# 2021-May-26 [Initial Version] - Shell Script for setup new server
+#-----------------------------------------------------------------------------
+#fonts color 字体颜色配置
+Red="\033[31m"
+Yellow="\033[33m"
+Blue="\033[36m"
+Green="\033[32m"
+
+RedBG="\033[41;37m"
+GreenBG="\033[42;37m"
+
+Font="\033[0m"
+#-----------------------------------------------------------------------------
+#notification information
+Info="${Green}[Message信息]${Font}"
+OK="${Green}[OK正常]${Font}"
+Error="${Red}[ERROR错误]${Font}"
+#-----------------------------------------------------------------------------
+#打印Info
+function print_info() {
+  echo -e "${Info} ${Blue} $1 ${Font}"
+}
+#-----------------------------------------------------------------------------
+#打印OK
+function print_ok() {
+  echo -e "${OK} ${Blue} $1 ${Font}"
+}
+#-----------------------------------------------------------------------------
+#打印错误
+function print_error() {
+  echo -e "${ERROR} ${RedBG} $1 ${Font}"
+}
+#-----------------------------------------------------------------------------
+#判定 成功 or 失败
+judge() {
+  if [[ 0 -eq $? ]]; then
+    print_ok "$1 <--- 完成"
+    sleep 1
+  else
+    print_error "$1 <--- 失败"
+    exit 1
+  fi
+}
+#-----------------------------------------------------------------------------
+# Installing and running the Node Exporter
+#-----------------------------------------------------------------------------
+#===== RHEL 7/8 | CentOS 7/8 | Oracle Linux 7/8 =====
+#-----------------------------------------------------------------------------
+# Security-Enhanced Linux
+# This guide is based on SELinux being disabled or in permissive mode. 
+# Steps to do this are as follows.
+#-----------------------------------------------------------------------------
+#DATE_TIME=`date "+%Y.%m.%d-%H:%M:%S"`
+#sudo script -aq setup-new-server.${DATE_TIME}.log
+# ./setup-new-server.sh >> setup-new-server.$`date "+%Y.%m.%d-%H:%M:%S"`.log 2>&1
+print_info "开始配置 Linux CentOS 7 服务器"
+sleep 1
+sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
+#judge "Step 1: Security-Enhanced Linux"
+print_info "Step 1: Security-Enhanced Linux <--- 完成"
+sleep 1
+#-----------------------------------------------------------------------------
+# Install Docker CE
+# https://docs.docker.com/engine/install/centos/
+#-----------------------------------------------------------------------------
+print_info "Step 2: Install Docker CE"
+sleep 1
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+judge "Step 2: 1/3 Uninstall old versions of Docker CE"
+sleep 1
+
+sudo yum -y install yum-utils
+sudo yum-config-manager \
+      --add-repo \
+      https://download.docker.com/linux/centos/docker-ce.repo
+
+judge "Step 2: 2/3 Set up the repository for Docker"
+sleep 1
+
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+judge "Step 2: 3/3 Install Docker Engine"
+sleep 1
+judge "Step 2: Install Docker CE"
+sleep 1
+#-----------------------------------------------------------------------------
+# Install Docker Compose
+# https://docs.docker.com/compose/install/
+#-----------------------------------------------------------------------------
+print_info "Step 3: Install Docker Compose"
+sleep 1
+sudo rm /usr/local/bin/docker-compose
+judge "Step 3: 1/3 Uninstallation"
+sleep 1
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+          -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+judge "Step 3: 2/3 Install Compose on Linux systems"
+sleep 1
+
+sudo docker-compose --version
+judge "Step 3: 3/3 Show docker-compose version"
+sleep 3
+
+judge "Step 3: Install Docker Compose"
+#-----------------------------------------------------------------------------
+# Install Git
+# https://git-scm.com
+#-----------------------------------------------------------------------------
+sudo yum -y install git
+judge "Step 4: Install Git"
+sleep 1
--- Shell/up-docker-compose.sh
+++ Shell/up-docker-compose.sh
@@ -16,6 +16,7 @@ sudo cp -f ~/git/toolbox/Shell/delete-all-in-one.sh ~/delete-all-in-one.sh
 sudo cp -f ~/git/toolbox/Shell/down-docker-compose.sh ~/down-docker-compose.sh
 sudo cp -f ~/git/toolbox/Shell/up-docker-compose.sh ~/up-docker-compose.sh
 sudo cp -f ~/git/toolbox/Shell/setup-new-server.sh ~/setup-new-server.sh
+sudo cp -f ~/git/toolbox/Shell/setup-new-s.sh ~/setup-new-s.sh
 sleep 1
 cd ~
 sudo chmod +x ./*.sh
