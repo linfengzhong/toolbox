fb59b43204d37dc1024e386e440981950074ca8f
linfengzhong <fred.zhong@outlook.com>
2021-06-25 17:28:09 +0800

    updates

--- Shell/smart-tool.sh
+++ Shell/smart-tool.sh
@@ -46,77 +46,175 @@ judge() {
 #定义变量
 WORKDIR="/root/git/toolbox/Docker/docker-compose/k8s-master.ml/"
 GITHUB_REPO="/root/git/toolbox/"
+EMAIL="fred.zhong@outlook.com"
+WEBSITE="k8s-master.ml"
+#-----------------------------------------------------------------------------
+#===== RHEL 7/8 | CentOS 7/8 | Rocky Linux 8 =====
+#-----------------------------------------------------------------------------
+# Security-Enhanced Linux
+# This guide is based on SELinux being disabled or in permissive mode. 
+# Steps to do this are as follows.
+#-----------------------------------------------------------------------------
+function turn_off_selinux () {
+  print_info "开始配置 Linux Rocky 8.4 / CentOS 8 服务器"
+  sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+  setenforce 0
+  #judge "Step 1: Security-Enhanced Linux"
+  print_info "Security-Enhanced Linux <--- 完成"
+}
 #-----------------------------------------------------------------------------#
 # Shutdown Docker Compose, Delete All-In-One folder & Docker Compose Up
 # 关闭docker-compose
 function shutdown_docker_compose () {
-print_info "Shutdown Docker Compose "
-print_info "关闭 Docker Compose VM "
-cd $WORKDIR
-sudo docker-compose down
-judge "关闭 Docker Compose VM "
+  print_info "Shutdown Docker Compose "
+  print_info "关闭 Docker Compose VM "
+  cd $WORKDIR
+  sudo docker-compose down
+  judge "关闭 Docker Compose VM "
 }
 #-----------------------------------------------------------------------------#
 # 查看Docker Images
 function show_docker_images () {
-sudo docker images
+  sudo docker images
 }
 #-----------------------------------------------------------------------------#
 # 列出所有运行的docker container
 function show_docker_container () {
-sudo docker container ps
+  sudo docker container ps
 }
 #-----------------------------------------------------------------------------#
 # 删除文件夹
 function delete_docker_compose_folder () {
-print_info "删除文件夹 "
-cd ~
-sudo rm -rf all-in-one/
-sudo rm -rf k8s-master.ml/
-judge "删除文件夹 "
+  print_info "删除文件夹 "
+  cd ~
+  sudo rm -rf all-in-one/
+  sudo rm -rf k8s-master.ml/
+  judge "删除文件夹 "
 }
 #-----------------------------------------------------------------------------#
 # 同步下载Git文件夹
 function github_pull () {
-print_info "更新同步 下载GitHub文件 -> Local Github Repo "
-cd $GITHUB_REPO
-sudo git pull
-#sleep 1
-#sudo cp -rf ~/git/toolbox/Docker/docker-compose/all-in-one/ ~/
-#sleep 1
-#sudo cp -rf ~/git/toolbox/Docker/docker-compose/k8s-master.ml/ ~/
-#sudo chown -R root:root ~/all-in-one/
-#sudo chown -R root:root ~/k8s-master.ml/
-judge "更新同步 下载GitHub文件 -> Local Github Repo "
+  print_info "更新同步 下载GitHub文件 -> Local Github Repo "
+  cd $GITHUB_REPO
+  sudo git pull
+  #sleep 1
+  #sudo cp -rf ~/git/toolbox/Docker/docker-compose/all-in-one/ ~/
+  #sleep 1
+  #sudo cp -rf ~/git/toolbox/Docker/docker-compose/k8s-master.ml/ ~/
+  #sudo chown -R root:root ~/all-in-one/
+  #sudo chown -R root:root ~/k8s-master.ml/
+  judge "更新同步 下载GitHub文件 -> Local Github Repo "
 }
 #-----------------------------------------------------------------------------#
 # 同步上传Git文件夹
 function github_push () {
-print_info "更新同步 上传Local Github Repo -> GitHub文件 "
-cd $GITHUB_REPO
-sudo git add .
-sudo git commit -m "sync_all_config_log_data"
-sudo git push
-judge "更新同步 上传Local Github Repo -> GitHub文件 "
-#sleep 1
-#sudo cp -rf ~/git/toolbox/Docker/docker-compose/all-in-one/ ~/
-#sleep 1
-#sudo cp -rf ~/git/toolbox/Docker/docker-compose/k8s-master.ml/ ~/
-#sudo chown -R root:root ~/all-in-one/
-#sudo chown -R root:root ~/k8s-master.ml/
+  print_info "更新同步 上传Local Github Repo -> GitHub文件 "
+  cd $GITHUB_REPO
+  sudo git add .
+  sudo git commit -m "sync_all_config_log_data"
+  sudo git push
+  judge "更新同步 上传Local Github Repo -> GitHub文件 "
+  #sleep 1
+  #sudo cp -rf ~/git/toolbox/Docker/docker-compose/all-in-one/ ~/
+  #sleep 1
+  #sudo cp -rf ~/git/toolbox/Docker/docker-compose/k8s-master.ml/ ~/
+  #sudo chown -R root:root ~/all-in-one/
+  #sudo chown -R root:root ~/k8s-master.ml/
 }
 #-----------------------------------------------------------------------------#
 # 启动docker-compose
 function start_docker_compose () {
-print_info "启动 Docker Compose "
-cd $WORKDIR
-sudo docker-compose build
-sudo docker-compose up -d
+  print_info "启动 Docker Compose "
+  cd $WORKDIR
+  sudo docker-compose build
+  sudo docker-compose up -d
 
-sudo rm -f /root/*.sh
-sudo cp -f /root/git/toolbox/Shell/smart-tool.sh /root/smart-tool.sh
-sudo chmod +x /root/*.sh
-judge "启动 Docker Compose "
+  sudo rm -f /root/*.sh
+  sudo cp -f /root/git/toolbox/Shell/smart-tool.sh /root/smart-tool.sh
+  sudo chmod +x /root/*.sh
+  judge "启动 Docker Compose "
+}
+#-----------------------------------------------------------------------------#
+# Install Prerequisite
+#-----------------------------------------------------------------------------#
+# 安装必要程序
+function install_prerequisite () {
+  yum -y install wget lsof tar unzip curl socat
+  judge "安装 wget lsof tar unzip curl socat"
+}
+#-----------------------------------------------------------------------------#
+# Install acme.sh
+#-----------------------------------------------------------------------------#
+function install_acme () {
+  print_info "Install acme.sh "
+  sudo curl https://get.acme.sh | sh -s email=$EMAIL
+  judge "安装 acme.sh "
+}
+#-----------------------------------------------------------------------------#
+# Generate CA
+#-----------------------------------------------------------------------------#
+function generate_ca () {
+  local WEBSITE=$1
+  print_info "----- 网站证书 ----"
+  sudo sh /root/.acme.sh/acme.sh  --issue  -d $WEBSITE --standalone
+  print_info "----- 网站证书 ----"
+  judge "Step 6: Install acme.sh"
+print_info "服务器外部 IP："
+sudo curl https://ipinfo.io/ip
+}
+#-----------------------------------------------------------------------------#
+# Install webmin
+# https://webmin.com
+#-----------------------------------------------------------------------------#
+function install_webmin () {
+  print_info "Install webmin "
+  (echo "[Webmin]
+  name=Webmin Distribution Neutral
+  baseurl=http://download.webmin.com/download/yum
+  enabled=1
+  gpgcheck=1
+  gpgkey=http://www.webmin.com/jcameron-key.asc" >/etc/yum.repos.d/webmin.repo;)
+  sleep 1
+  sudo yum -y install webmin
+  judge "Install webmin "
+}
+#-----------------------------------------------------------------------------#
+# Install Git
+# https://git-scm.com
+#-----------------------------------------------------------------------------#
+function install_git () {
+  print_info "Install Git "
+  sudo yum -y install git
+  judge "Install Git "
+}
+#-----------------------------------------------------------------------------#
+# Install Docker CE
+# https://docs.docker.com/engine/install/centos/
+#-----------------------------------------------------------------------------#
+function install_docker () {
+  print_info "Install Docker CE "
+  sudo yum -y remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+  judge "1/3 Uninstall old versions of Docker CE "
+  sudo yum -y install yum-utils
+  sudo yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+
+  judge "2/3 Set up the repository for Docker "
+
+  sudo yum -y install docker-ce docker-ce-cli containerd.io
+  sudo systemctl start docker
+  sudo systemctl enable docker
+
+  judge "3/3 Install Docker Engine "
+  judge "Install Docker CE "
 }
 #-----------------------------------------------------------------------------#
 # 展示命令选项
@@ -126,7 +224,14 @@ Usage: smart-tool.sh down|up
               all
               delete
               git-push|git-pull
-              status"
+              status
+              turn-off-selinux
+              install-prerequisite
+              install-acme|generate-CA
+              install-webmin
+              install-git
+              install-docker
+              install-all"
 }
 
 RC=0
@@ -155,6 +260,36 @@ case "x$1" in
   "xstatus")
     show_docker_images
     show_docker_container
+    ;;
+  "xturn-off-selinux")
+    turn_off_selinux
+    ;;
+  "xinstall-prerequisite")
+    install_prerequisite
+    ;;
+  "xinstall-acme")
+    install_acme
+    ;;
+  "xgenerate-CA")
+    generate_CA
+    ;;
+  "xinstall-webmin")
+    install_webmin
+    ;;
+  "xinstall-git")
+    install_git
+    ;;
+  "xinstall-docker")
+    install_docker
+    ;;
+  "xinstall-all")
+    turn_off_selinux
+    install_prerequisite
+    install_acme
+    generate_CA
+    install_webmin
+    install_git
+    install_docker
     ;;
   *)
     usage
