7a89571b87dd9194892bee55ac7026cce230aef1
linfengzhong <fred.zhong@outlook.com>
2021-06-28 17:31:02 +0800

    new versino

--- Shell/smart-tool-v2.sh
+++ Shell/smart-tool-v2.sh
@@ -360,9 +360,10 @@ function updateSmartTool() {
 
 	local version=$(cat /etc/smart-tool/smart-tool-v2.sh | grep 'SmartTool：v' | awk -F "[v]" '{print $2}' | tail -n +2 | head -n 1 | awk -F "[\"]" '{print $1}')
 	print_info "---> 更新前版本:${version}\n"
-
+	version=""
+	
 	rm -rf /etc/smart-tool/smart-tool-v2.sh
-	echoContent skyBlue  "---> 开始下载："
+	echoContent skyBlue "开始下载：\c"
 	if wget --help | grep -q show-progress; then
 		wget -c -q --show-progress -P /etc/smart-tool/ -N --no-check-certificate "https://raw.githubusercontent.com/linfengzhong/toolbox/main/Shell/smart-tool-v2.sh"
   else
@@ -534,7 +535,7 @@ function menu() {
 	clear
 	cd "$HOME" || exit
 	echoContent red "\n=============================================================="
-	echoContent green "SmartTool：v0.039"
+	echoContent green "SmartTool：v0.040"
 	echoContent green "Github：https://github.com/linfengzhong/toolbox"
 	echoContent green "初始化服务器、安装Docker、执行容器"
 	echoContent green "当前系统Linux 版本 : \c" 
