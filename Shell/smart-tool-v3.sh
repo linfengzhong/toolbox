806deb5d6766d7e983476f8d645b328393b76ea8
linfengzhong <fred.zhong@outlook.com>
2021-07-10 17:31:00 +0800

    updates

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -105,6 +105,37 @@ function judge() {
 	fi
 }
 #-----------------------------------------------------------------------------#
+# 输出带颜色内容 字体颜色配置
+function echoContent() {
+	case $1 in
+		# 红色
+	"red")
+		# shellcheck disable=SC2154
+		${echoType} "\033[31m${printN}$2 \033[0m"
+		;;
+		# 天蓝色
+	"skyBlue")
+		${echoType} "\033[1;36m${printN}$2 \033[0m"
+		;;
+		# 绿色
+	"green")
+		${echoType} "\033[32m${printN}$2 \033[0m"
+		;;
+		# 白色
+	"white")
+		${echoType} "\033[37m${printN}$2 \033[0m"
+		;;
+		# 洋红
+	"magenta")
+		${echoType} "\033[31m${printN}$2 \033[0m"
+		;;
+		# 黄色
+	"yellow")
+		${echoType} "\033[33m${printN}$2 \033[0m"
+		;;
+	esac
+}
+#-----------------------------------------------------------------------------#
 # Install acme.sh
 function install_acme () {
 	print_info "Install acme.sh "
@@ -454,37 +485,6 @@ function checkSystem() {
 		cat /proc/version
 		exit 0
 	fi
-}
-#-----------------------------------------------------------------------------#
-# 输出带颜色内容 字体颜色配置
-function echoContent() {
-	case $1 in
-		# 红色
-	"red")
-		# shellcheck disable=SC2154
-		${echoType} "\033[31m${printN}$2 \033[0m"
-		;;
-		# 天蓝色
-	"skyBlue")
-		${echoType} "\033[1;36m${printN}$2 \033[0m"
-		;;
-		# 绿色
-	"green")
-		${echoType} "\033[32m${printN}$2 \033[0m"
-		;;
-		# 白色
-	"white")
-		${echoType} "\033[37m${printN}$2 \033[0m"
-		;;
-		# 洋红
-	"magenta")
-		${echoType} "\033[31m${printN}$2 \033[0m"
-		;;
-		# 黄色
-	"yellow")
-		${echoType} "\033[33m${printN}$2 \033[0m"
-		;;
-	esac
 }
 #-----------------------------------------------------------------------------#
 # Generate CA
