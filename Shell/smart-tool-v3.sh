f531cfb928f74e92e55ef25ac888054b2c3f4576
linfengzhong <fred.zhong@outlook.com>
2021-07-10 21:50:16 +0800

    updates

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -754,20 +754,9 @@ server {
         proxy_pass http://gitea:3000/;
     }
 
-	location /jenkins {
-		proxy_pass http://jenkins:8080;
-		proxy_http_version  1.1;
-		proxy_cache_bypass  $http_upgrade;
-
-		proxy_set_header Upgrade           $http_upgrade;
-		proxy_set_header Connection        "upgrade";
-		proxy_set_header Host              $host;
-		proxy_set_header X-Real-IP         $remote_addr;
-		proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
-		proxy_set_header X-Forwarded-Proto $scheme;
-		proxy_set_header X-Forwarded-Host  $host;
-		proxy_set_header X-Forwarded-Port  $server_port;
-	}
+    location /jenkins/ {
+        proxy_pass http://jenkins:8080;
+    }
 }
 EOF
 	cat /etc/fuckGFW/nginx/conf.d/${currentHost}.conf
