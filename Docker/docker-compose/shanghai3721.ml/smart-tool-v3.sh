c08a06550158b06a305afe142de6148978e2a25a
linfengzhong <fred.zhong@outlook.com>
2021-07-11 20:20:51 +0800

    updates

--- Docker/docker-compose/shanghai3721.ml/smart-tool-v3.sh
+++ Docker/docker-compose/shanghai3721.ml/smart-tool-v3.sh
@@ -966,8 +966,6 @@ services:
             TZ: Asia/Shanghai
         ports:
             - 80:80
-        expose:
-            - 31300
         volumes: 
             - /etc/fuckGFW/nginx/conf.d/:/etc/nginx/conf.d
             - /etc/fuckGFW/website/html:/usr/share/nginx/html
