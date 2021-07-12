bb9e7b54e202287222f3aec56e0a2375f67e73f7
linfengzhong <fred.zhong@outlook.com>
2021-07-12 15:27:53 +0800

    updates

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -806,7 +806,7 @@ function generate_xray_conf {
             "path": "/rdxryws",
             "dest": 31297,
             "xver": 1
-          }
+          },
           {
             "path": "/rdtov2rayxyz",
             "dest": "v2ray:443",
@@ -2339,7 +2339,7 @@ function menu() {
 	clear
 	cd "$HOME" || exit
 	echoContent red "\n=================================================================="
-	echoContent green "SmartTool：v0.228"
+	echoContent green "SmartTool：v0.229"
 	echoContent green "Github：https://github.com/linfengzhong/toolbox"
 	echoContent green "logserver：https://github.com/linfengzhong/logserver"
 	echoContent green "初始化服务器、安装Docker、执行容器 on \c" 
