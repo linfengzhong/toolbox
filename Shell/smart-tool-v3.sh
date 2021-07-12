9cff99d339bc7c1cae86b57fd7a5c2db4e472f60
linfengzhong <fred.zhong@outlook.com>
2021-07-12 15:25:17 +0800

    updates

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -2339,7 +2339,7 @@ function menu() {
 	clear
 	cd "$HOME" || exit
 	echoContent red "\n=================================================================="
-	echoContent green "SmartTool：v0.227"
+	echoContent green "SmartTool：v0.228"
 	echoContent green "Github：https://github.com/linfengzhong/toolbox"
 	echoContent green "logserver：https://github.com/linfengzhong/logserver"
 	echoContent green "初始化服务器、安装Docker、执行容器 on \c" 
