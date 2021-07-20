4cffe6e5075cc8bb9e22ec30e3a55bb4bf049cc6
linfengzhong <fred.zhong@outlook.com>
2021-07-21 06:38:43 +0800

    updates

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -1975,12 +1975,15 @@ function customize_nagios_server_myservers {
 			# 双引号可以用shell变量
 			sed -i "s/NagiosClientDomain/$NagiosClientDomain1/g" /usr/local/nagios/etc/objects/myservers/${NagiosClientDomain1}.cfg
 
-			read -r -p "请输入Nagios client IP : " NagiosClientIP1
-			if [ $NagiosClientIP1 ]; then
+			NagiosClientIP1='ping ${NagiosClientDomain1} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}''
+			read -r -p "请确认IP地址 ${NagiosClientIP1} 是否正确：[y/n] " anwser_y_n
+			if [ anwser_y_n="y" or anwser_y_n="Y" ]; then
 				# 双引号可以用shell变量
 				sed -i "s/NagiosClientIP/$NagiosClientIP1/g" /usr/local/nagios/etc/objects/myservers/${NagiosClientDomain1}.cfg
 				chown nagios:nagios /usr/local/nagios/etc/objects/myservers/${NagiosClientDomain1}.cfg
 				chmod 777 /usr/local/nagios/etc/objects/myservers/${NagiosClientDomain1}.cfg
+			else
+				print_error "请重新输入正确的服务器域名！"
 			fi
 		fi
 	else
