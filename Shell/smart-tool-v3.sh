76dd9808780c9b28fd92be2d9c035f15fe72c9ca
linfengzhong <fred.zhong@outlook.com>
2021-07-21 08:34:14 +0800

    up

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -288,12 +288,14 @@ function install_nginx () {
 	if cat /etc/nginx/nginx.conf | grep "listen       7080 default_server;" ; then
 		print_error "已经设置端口：7080，无需重复设置！"
 	else
+		print_info "设置IPv4 端口为 7080"
 		sed -i 's!listen       80 default_server;!listen       7080 default_server;!g' /etc/nginx/nginx.conf
 	fi
 
 	if cat /etc/nginx/nginx.conf | grep "listen       [::]:7080 default_server;" ; then
 		print_error "已经设置端口：7080，无需重复设置！"
 	else
+		print_info "设置IPv6 端口为 7080"
 		sed -i 's!listen       [::]:80 default_server;!listen       [::]:7080 default_server;;!g' /etc/nginx/nginx.conf
 	fi
 	systemctl reload nginx
