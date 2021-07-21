61af5b08088c336753d0e40ae16253ace87f59ee
linfengzhong <fred.zhong@outlook.com>
2021-07-21 20:12:57 +0800

    updates

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -2344,9 +2344,9 @@ function customize_nagios_client_copy_libexec {
 		print_error "请先Git同步toolbox到本地，再进行设置！"
 		print_info "Plan B: wget 文件到Libexec"
 		wget -c -q -P /usr/local/nagios/libexec/check_cpu_stats.sh -N --no-check-certificate "https://raw.githubusercontent.com/linfengzhong/toolbox/main/Nagios/Libexec/check_cpu_stats.sh"
-		wget -c -q -P /usr/local/nagios/libexec/check_cpu_stats.sh -N --no-check-certificate "https://raw.githubusercontent.com/linfengzhong/toolbox/main/Nagios/Libexec/check_kernel"
-		wget -c -q -P /usr/local/nagios/libexec/check_cpu_stats.sh -N --no-check-certificate "https://raw.githubusercontent.com/linfengzhong/toolbox/main/Nagios/Libexec/check_mem.pl"
-		wget -c -q -P /usr/local/nagios/libexec/check_cpu_stats.sh -N --no-check-certificate "https://raw.githubusercontent.com/linfengzhong/toolbox/main/Nagios/Libexec/check_service.sh"	
+		wget -c -q -P /usr/local/nagios/libexec/check_kernel -N --no-check-certificate "https://raw.githubusercontent.com/linfengzhong/toolbox/main/Nagios/Libexec/check_kernel"
+		wget -c -q -P /usr/local/nagios/libexec/check_mem.pl -N --no-check-certificate "https://raw.githubusercontent.com/linfengzhong/toolbox/main/Nagios/Libexec/check_mem.pl"
+		wget -c -q -P /usr/local/nagios/libexec/check_service.sh -N --no-check-certificate "https://raw.githubusercontent.com/linfengzhong/toolbox/main/Nagios/Libexec/check_service.sh"	
 		chmod 755 /usr/local/nagios/libexec/*.*
 	fi
 }
@@ -3282,7 +3282,7 @@ function menu() {
 		;;
 	esac
 }
-SmartToolVersion=v0.313
+SmartToolVersion=v0.314
 cleanScreen
 initVar $1
 set_current_host_domain
