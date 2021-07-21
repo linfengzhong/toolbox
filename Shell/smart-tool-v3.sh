20534f88b47c2b78211ff61e7d3b441147ecfe4c
linfengzhong <fred.zhong@outlook.com>
2021-07-21 13:06:37 +0800

    updates

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -2163,7 +2163,7 @@ function customize_nagios_server_myservers_show {
 			arr=(${arr[*]} $entry)
 		fi
 	done
-	echo ${arr[*]}
+	print_info "${arr[*]} \n"
 	print_complete "Nagios Myservers "
 }
 #-----------------------------------------------------------------------------#
