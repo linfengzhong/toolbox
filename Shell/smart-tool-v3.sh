36db76a02c647ad84eb2f44cbed24295949b2695
linfengzhong <fred.zhong@outlook.com>
2021-07-24 08:58:41 +0800

    updates

--- Shell/smart-tool-v3.sh
+++ Shell/smart-tool-v3.sh
@@ -2682,7 +2682,7 @@ function customize_nagios_client_copy_libexec {
 		chmod 755 /usr/local/nagios/libexec/*.*
 	else
 		print_error "请先Git同步toolbox到本地，再进行设置！"
-		print_info "Plan B: wget 文件到Libexec"
+		print_error "Plan B: wget 文件到Libexec"
 		
 #		rm -f /usr/local/nagios/libexec/check_cpu_stats.sh
 #		rm -f /usr/local/nagios/libexec/check_kernel
